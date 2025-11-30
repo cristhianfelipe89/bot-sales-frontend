e681b76955cd803bb9a593ed0fea0de476248482
cristhianfelipe89 <cristhianfelipe89@gmail.com>
2025-11-30 12:49:11 -0500

    crear archivo necesario para github actions

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 import axios from "axios";
-const BASE = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+const BASE = import.meta.env.VITE_API_URL;
 const api = axios.create({ baseURL: BASE, withCredentials: true });
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
